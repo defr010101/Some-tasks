ebcc636f400650492524969ff8f8da25f57f54cd
defr010101 <defr010101@yandex.ru>
2024-01-01 10:54:05 +0300

    Main.java been changed

--- deque/Main.java
+++ deque/Main.java
@@ -1,16 +1,18 @@
 package deque;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayDeque<String> deque = new ArrayDeque<>();
+        Deque<String> deque = new ArrayDeque<>();
         deque.add("1");
         deque.add("2");
         deque.add("3");
+	deque.add("4");
 
-        for (String item : deque.reversed()) {
-            System.out.println(item);
+        for (String element : deque.reversed()) {
+            System.out.println(element);
         }
     }
 }
